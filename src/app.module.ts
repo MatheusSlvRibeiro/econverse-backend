ff3deb8401cd2cc66387878a4d71508feb63198f
Matheus <silva.contadev@gmail.com>
2026-09-09 20:45:41 -0300

    feat: endpoint GET /categories
    CategoriesModule/CategoriesController/CategoriesService retornando
    todas as categorias do banco.
    
    Refs #6

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { ProductsModule } from './products/products.module.js';
+import { CategoriesModule } from './categories/categories.module.js';
 
 @Module({
-  imports: [PrismaModule, ProductsModule],
+  imports: [PrismaModule, ProductsModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
